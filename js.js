166ba8194ad4d3083f846b428ce3c375ccf732aa
amalyakubov <129682707+amalyakubov@users.noreply.github.com>
2023-04-18 18:58:54 +0200

    Implement the code that plays one round of a game
    Create the variable 'computerSelection' and set it to the result of calling
    'getComputerSelection()' function.
    Create the a variable 'computerSelectionLoweCase' to be the lowercased
    version of the computerSelection, in order for the code to check all
    possible variations of cases in the computerSelection string.
    Create the variable 'playerSelection' and set it to be the lowercased
    version of the string 'rock'
    Create a function 'playARound()' that plays one round of the game.
    Inside of the 'playARound()' function, create another function called
    'tieLoseWinIndex()' in order to improve the readability of the code.
    The function returns an integer from -1 to 1 that represents ; a loss
    for the player, a tie, or a victory for the player.
    Create the variable 'index' and set to be equal the result of the calling
    the function 'tieLoseWinIndex()' with arguments 'computerSelectionLoweCase'
    and 'playerSelection'.
    Create a check for the value of the index to return the end message.

--- js.js
+++ js.js
@@ -1,3 +1,5 @@
+let computerSelection =  getComputerChoice();
+
 function getComputerChoice() {
     let choice = 0;
     choice = Math.floor(Math.random() * 3);
@@ -9,3 +11,28 @@ function getComputerChoice() {
         return 'Scissors';
     }
  }
+
+ let computerSelectionLowerCase = computerSelection.toLowerCase();
+ const playerSelection = 'rock'.toLowerCase();
+
+function playRound() {
+    function tieLoseWinIndex (computerSelection, playerSelection) {
+        if (computerSelection === playerSelection) {
+            return 0;
+        } else if (computerSelection === 'rock' && playerSelection === 'scissors' || computerSelection === 'paper' && playerSelection === 'rock' || computerSelection === 'scissors' && playerSelection === 'paper') {
+            return -1;
+        } else {
+            return 1;
+        } 
+}
+let index = tieLoseWinIndex(computerSelectionLowerCase, playerSelection);
+    if (index === 0) {
+        return 'Tie';
+    } else if (index === 1) {
+        return 'You won!';
+    } else {
+        return 'You lost.';
+    }
+}
+
+console.log(playRound());
